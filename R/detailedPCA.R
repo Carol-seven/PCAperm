749d5055cde9df63669a91545dce0525cad7dff5
Carol-seven <542046605@qq.com>
2023-12-17 19:36:51 -0500

    update detailedPCA

--- R/detailedPCA.R
+++ R/detailedPCA.R
@@ -13,8 +13,6 @@
 #' be shifted to be zero centered.
 #'
 #' @return A list containing the following components: \describe{
-#' \item{X}{The value of the rotated data (the centred (and scaled if requested) data
-#' multiplied by the rotation matrix).}
 #' \item{sdev}{The standard deviations of the principal components, calculated as the
 #' square roots of the eigenvalues of the covariance/correlation matrix. The calculation
 #' is actually done with the singular values of the data matrix.}
@@ -22,10 +20,12 @@
 #' variance of the principal components. The calculation is actually done with the
 #' singular values of the data matrix.}
 #' \item{pctvar}{The percentage of variance explained by each principal component.}
+#' \item{cumvar}{The cumulative percentage of variance explained by each principal component.}
 #' \item{rotation}{The matrix of variable loadings (i.e., a matrix whose columns contain
 #' the eigenvectors).}
-#' \item{indexload}{The index of loadings for the variables.}
-#' \item{correlation}{The correlations of the variables with each principal component.}
+#' \item{score}{The value of the rotated data (the centred (and scaled if requested) data
+#' multiplied by the rotation matrix), representing the scores of the supplied data on all
+#' principal components.}
 #' \item{Psi}{The Psi index, which depends on the magnitude of the eigenvalues taken from
 #' the correlation matrix of the data set.
 #' \deqn{\Psi = \sum(\lambda_i-1)^2,}
@@ -34,10 +34,12 @@
 #' the variables.
 #' \deqn{\Phi = \sqrt{\frac{\sum\lambda_i^2-p}{p(p-1)}},}
 #' where \eqn{\lambda} is the eigenvalue, and \eqn{p} is the number of variables.}
+#' \item{correlation}{The correlations of the principal components with the variables
+#' (Jackson, 1991).}
+#' \item{indexload}{The index of the loadings (Vieira, 2012).}
 #' \item{center}{The centering used, or \code{FALSE}.}
 #' \item{scale}{The scaling used, or \code{FALSE}.}
 #' }
-#'
 #'
 #' @references \itemize{
 #' \item Gleason, T. C. and Staelin, R. (1975).
@@ -48,6 +50,9 @@
 #' \item Becker, R. A., Chambers, J. M. and Wilks, A. R. (1988).
 #' The New S Language. Wadsworth & Brooks/Cole.
 #' \emph{Computer Science Series, Pacific Grove, CA}.
+#' \item Jackson, J. Edward. (1991).
+#' A User's Guide to Principal Components.
+#' John Wiley & Sons, New York, USA.
 #' \item Vieira, Vasco M. N. C. S. (2012).
 #' Permutation Tests to Estimate Significances on Principal Components Analysis.
 #' \emph{Computational Ecology and Software}, 2(2), 103--123.
@@ -79,12 +84,12 @@ detailedPCA <- function(X, center = TRUE, scale = TRUE) {
 
   ## the scaling used
   scaling.used <- attr(X, "scaled:scale")
-  if (is.null(scaling.used)) {
-    scaling.used <- FALSE
-  }
-
+  ## check if any variable has zero variance after scaling
   if(any(scaling.used == 0)) {
     stop("Cannot rescale a constant/zero column to unit variance!")
+  }
+  if (is.null(scaling.used)) {
+    scaling.used <- FALSE
   }
 
   ## singular value decomposition of matrix X
@@ -107,15 +112,15 @@ detailedPCA <- function(X, center = TRUE, scale = TRUE) {
   ## the percentage of variance explained by each eigenvalue
   pctvar <- eigval / sum(eigval) * 100
 
+  ## the cumulative percentage of variance explained by each eigenvalue
+  cumvar <- cumsum(pctvar)
+
   ## the matrix of variable loadings
   dimnames(S$v) <- list(colnames(X), paste0("PC", seq_len(ncol(S$v))))
   rotation <- S$v
 
-  ## the index loadings of the variables
-  indexload <- t(rotation)^2 * eigval^2
-
-  ## the correlations of the variables
-  correlation <- t(rotation) * sqrt(eigval)
+  ## the scores of the supplied data on the PCs
+  score <- X %*% rotation
 
   ## Psi index
   Psi <- sum((eigval - 1)^2)
@@ -123,11 +128,17 @@ detailedPCA <- function(X, center = TRUE, scale = TRUE) {
   ## Phi statistic
   Phi <- sqrt((sum(eigval^2) - p) / (p*(p-1)))
 
-  ## rotated variables
-  X <- X %*% rotation
+  ## the correlations of the PCs with the variables
+  correlation <- t(rotation) * sqrt(eigval)
 
-  return(list(X = X, sdev = sdev, eigval = eigval, pctvar = pctvar,
-              rotation = rotation, indexload = indexload, correlation = correlation,
-              Psi = Psi, Phi = Phi, center = centering.used, scale = scaling.used))
+  ## the index of the loadings
+  indexload <- t(rotation)^2 * eigval^2
+
+  return(list(sdev = sdev, eigval = eigval,
+              pctvar = pctvar, cumvar = cumvar,
+              rotation = rotation, score = score,
+              Psi = Psi, Phi = Phi,
+              correlation = correlation, indexload = indexload,
+              center = centering.used, scale = scaling.used))
 }
 
